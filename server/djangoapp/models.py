43fadd6655660dd7aa187aecdb5d3c190f1acbab
Conor McGrath <conormcgrath@gmail.com>
2023-11-29 11:42:19 -0500

    Build CarModel and CarMake Django Models
    Signed-off-by: Conor McGrath <conormcgrath@gmail.com>

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=200)
+    description = models.CharField(null=False, max_length=200)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -18,9 +24,29 @@ from django.utils.timezone import now
 # - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
 # - Year (DateField)
 # - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField(default=0)
+    name = models.CharField(null=False, max_length=200)
 
-
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    TYPE_CHOICES = [
+        (SEDAN, 'sedan'),
+        (SUV, 'suv'),
+        (WAGON, 'wagon')
+    ]
+    type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+    year = models.DateField(null=True)
+    # - __str__ method to print a car make object
+    def __str__(self):
+        return self.name
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
